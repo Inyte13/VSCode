f6d7e8ecd25fd1c961b15f2714f9a57f4d60a2f4
Luis García <luisgardaz13@gmail.com>
2025-08-31 10:27:56 -0500

    Actualización

---
+++ Python/Teoría/01_Basic.py
@@ -0,0 +1,19 @@
+print("Hola", "Mundo", sep="") # (HolaMundo) por lo general es sep=" "
+print("Sin salto de línea", end=" ") # (Sin salto de línea Esto) 
+print("Esto")
+print(type(None)) # (NoneType) Existe un no valors
+print(int("100")) # Para convertir el type
+print(bool(0)) # El único int False es el 0
+print(bool("")) # El único  str False es el vacío
+print(round(2.5)) # (2) Cuando esta justo en el medio, devolvera al PAR más cercano
+# Tipado Dinámico
+name="Luis"
+print(name)
+name=19
+print(name)
+# No recomendado para asignar variables
+a1, a2="N", "O"
+print(a1,a2, sep="")
+# Para las constantes deberá usarse Uppercase
+PI=3.1416
+
--- Python/Teoría/01_Teoría.py
+++
@@ -1,3 +0,0 @@
-print("Hola", "Mundo", sep="") # (HolaMundo) por lo general es sep=" "
-print("Sin salto de línea", end=" ") # (Sin salto de línea Esto) 
-print("Esto")
