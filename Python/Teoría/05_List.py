49abf752776277c84a78ae0acaad24ac7fdb89dc
Luis García <luisgardaz13@gmail.com>
2025-09-01 14:18:19 -0500

    Actualización

---
+++ Python/Teoría/05_List.py
@@ -0,0 +1,17 @@
+lista2=["Plátano", "Manzana", "Piña"] # Lista de str
+lista3=[False, 1, "2", 3.14] # Lista mixta
+lista_vacia=[]
+matriz=[[1,2], [3,4]]
+print(lista2[-1]) # Podemos acceder desde atrás
+lista1=[1, 2, 3, 4, 5] # Lista de int
+# Slicing
+print(lista1[1:4]) # [2, 3, 4]
+print(lista1[:4]) # [1, 2, 3, 4]
+print(lista1[3:]) # [4, 5]
+print(lista1[::2]) # [1, 3, 5]
+print(lista1[::-1]) # [5, 4, 3, 2, 1]
+# Copiar la lista
+print(lista1[:]) # [1, 2, 3, 4, 5]
+lista1+=[6,7] # Agregando valores
+# Longitud de la lista
+print(len(lista1))
---
+++ Python/Teoría/06_Ejercicios.py
@@ -0,0 +1,23 @@
+# E.1
+# mensaje=["C", "o", "d", "i", "g", "o", " ", "s", "e", "c", "r", "e", "t", "o"]
+# print(mensaje[7:])
+# E.2
+# numeros=[10, 20, 30, 40, 50]
+# numeros[0]=numeros[-1], numeros[0]
+# print(numeros)
+# E.3
+# pan_arriba=["Pan arriba"]
+# ingredientes=["Jamón", "Queso", "Tomate"]
+# pan_abajo=["Pan abajo"]
+# sandwich=pan_arriba+ingredientes+pan_abajo
+# E.4
+# list=[1, 2, 3]
+# new_list=list+list
+# print(new_list)
+# E.5
+# lista=[1,2,3,4,5,6,7]
+# print(lista[len(lista)//2])
+# E.6
+# lista=[1,2,3,4,5,6]
+# mitad=len(lista)//2
+# print(lista[:mitad][::-1]+lista[mitad:])
