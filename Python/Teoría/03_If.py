ffd68e70df9988598ebad6a9d4c49ae3806164ba
Luis García <luisgardaz13@gmail.com>
2025-08-31 21:08:31 -0500

    Actualización

---
+++ Python/Teoría/03_If.py
@@ -0,0 +1,11 @@
+edad=int(input("Ingrese su edad: "))
+if edad>18:
+    print("Eres mayor de edad")
+elif edad==18:
+    print("Tienes justo 18 años")
+else:
+    print("Eres menor de edad")
+# Condición ternaria, un if else en una solo línea
+print("Eres mayor de edad") if edad>=18 else print("Eres menor de edad")
+# [Si cumple la condicion] if [condición] else [Si no cumple]
+    
---
+++ Python/Teoría/04_Ejercicios.py
@@ -0,0 +1,52 @@
+# E.1
+# num1, num2=input("Ingrese dos números: ").split()
+# num1=int(num1)
+# num2=int(num2)
+# if num1>num2:
+#     print(f"{num1} es mayor que {num2}")
+# elif num2>num1:
+#     print(f"{num2} es mayor que {num1}")
+# else:
+#     print("Son iguales")
+# E.2
+# num1, num2=input("Ingrese dos números: ").split()
+# num1=float(num1)
+# num2=float(num2)
+# ope=input("Escriba una operación (+, -, *, /): ")
+# if ope=="+":
+#     print(num1+num2)
+# elif ope=="-":
+#     print(num1-num2)
+# elif ope=="*":
+#     print(num1*num2)
+# elif ope=="/":
+#     if num2==0:
+#         print("No se puede dividir entre 0")
+#     else:
+#         print(num1/num2)
+# else:
+#     print("Ingrese una operación válida")
+# E.3
+# year=int(input("Ingrese el año: "))
+# if (year%4==0 and year%100!=0) or year%400==0:
+#     print(f"El {year} es bisiesto")
+# else:
+#     print(f"El {year} no es bisiesto")
+# E.4
+# age=int(input("Ingresa tu edad: "))
+# etapa=""
+# if 65<=age:
+#     etapa="Adulto mayor"
+# elif 17<=age:
+#     etapa="Adulto"
+# elif 13<=age:
+#     etapa="Adolescente"
+# elif 3<=age:
+#     etapa="Niño"
+# elif 0<=age:
+#     etapa="Bebé"
+# else:
+#     print("Ingrese una edad válida")
+# print(etapa)
+
+
