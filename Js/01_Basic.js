81b7d12eed7da4092717a2467039b01e34a8f73a
Luis García <luisgardaz13@gmail.com>
2025-09-10 22:14:37 -0500

    Actualización

---
+++ Js/01_Basic.js
@@ -0,0 +1,5 @@
+var name="Luis" // Forma antigua de asignar una variable (No recomendado)
+let age=19 // Solo accesibles dentro de un bloque
+const pi=3.1416 // Constante, no se puede cambiar
+let nothing // Variable no asignada es "undefined"
+let nulo=null // Intencionadamente tiene el valor de "null"
