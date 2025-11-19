5ca629aa6de22efef8d08ee2a54b5ece52688d77
Inyte13 <luisgardaz13@gmail.com>
2025-11-19 11:10:18 -0500

    react-petshop

---
+++ PetShop/src/App.jsx
@@ -0,0 +1,13 @@
+import{Header}from "./components/Header.jsx"
+import{Footer}from "./components/Footer.jsx"
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+    <Header/>
+    <Footer/>
+    </>
+  )
+}
+
