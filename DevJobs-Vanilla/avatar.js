8247058ecaece8366cfc643cc4a715726fb41cc6
Inyte13 <luisgardaz13@gmail.com>
2025-12-31 21:53:22 -0500

    -oriana

--- DevJobs-Vanilla/avatar.js
+++ DevJobs-Vanilla/avatar.js
@@ -6,7 +6,7 @@ class DevJobsAvatar extends HTMLElement{ //
   render(){
     // Usamos shadowRoot para acceder al Shadow DOM
     this.shadowRoot.innerHTML=`
-    <img src="https://media.licdn.com/dms/image/v2/D4E03AQH2aKgTN3Xv8Q/profile-displayphoto-shrink_200_200/B4EZTuSgpTHgAY-/0/1739164618694?e=2147483647&v=beta&t=ctoIzpUijuUHWbhXd_C7qiSptzgVkzRBB1UNNye17U8" alt="Avatar de Oriana" class="avatar" style="width: 40px; height: 40px; border-radius: 100%;"/>`
+    <img src="" alt="Avatar" class="avatar" style="width: 40px; height: 40px; border-radius: 100%;"/>`
   }
 // Cuando el render se añada al DOM, llamamos a render()
   connectedCallback(){
