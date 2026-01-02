67c1fae7544b537b9d151fed771cbb9edb1ce979
Inyte13 <luisgardaz13@gmail.com>
2026-01-01 22:35:54 -0500

    NavLink y su isActive

--- DevJobs/src/components/Header.jsx
+++ DevJobs/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { Link } from './Link'
 import styles from './Header.module.css'
-export function Header () {
+import { NavLink } from 'react-router'
+export function Header ({ isLogueado, onLogin, onLogout }) {
   return (
     <header className={styles.header}>
       <div className={styles.brand}>
@@ -11,16 +12,27 @@ export function Header () {
       </div>
       <nav>
         <ul>
-          <li><Link href='/busqueda'>Empleos</Link></li>
+          <li>
+            <NavLink
+              className={({ isActive }) => isActive ? styles.isActive : ''}
+              to='/busqueda'
+            >
+              Empleos
+            </NavLink>
+          </li>
           <li><a href='#empresas'>Empresas</a></li>
           <li><a href='#salarios'>Salarios</a></li>
         </ul>
       </nav>
       <div className={styles.actions}>
-        <a href='#publicar'>Subir CV</a>
         <div>
           {/* <devjobs-avatar></devjobs-avatar> */}
         </div>
+        {
+          isLogueado
+            ? <button onClick={onLogout}>Cerrar sesión</button>
+            : <button onClick={onLogin}>Iniciar sesión</button>
+        }
       </div>
     </header>
   )
