86d35bcb67d133e9f584035eabff01a076fcac1a
Inyte13 <luisgardaz13@gmail.com>
2026-01-01 22:35:31 -0500

    lazy, para solo cargar lo necesario

--- DevJobs/src/App.jsx
+++ DevJobs/src/App.jsx
@@ -1,19 +1,34 @@
 import { Routes, Route } from 'react-router'
 import { Header } from './components/Header.jsx'
 import { Footer } from './components/Footer.jsx'
-import { Home } from './pages/Home.jsx'
-import { Busqueda } from './pages/Busqueda.jsx'
-import { NotFoundPage } from './pages/NotFoundPage.jsx'
-import { Job } from './pages/Job.jsx'
+import { lazy, useState } from 'react'
+
+const Home = lazy(() => import('./pages/Home.jsx'))
+const Busqueda = lazy(() => import('./pages/Busqueda.jsx'))
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage.jsx'))
+const Job = lazy(() => import('./pages/Job.jsx'))
 
 export default function App () {
+  const [isLogueado, setIsLogueado] = useState(true)
+  const manejarLogin = () => {
+    setIsLogueado(true)
+  }
+
+  const manejarLogout = () => {
+    setIsLogueado(false)
+  }
+
   return (
     <>
-      <Header />
+      <Header
+        isLogueado={isLogueado}
+        onLogin={manejarLogin}
+        onLogout={manejarLogout}
+      />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/busqueda' element={<Busqueda />} />
-        <Route path='/jobs/:id' element={<Job />} />
+        <Route path='/jobs/:id' element={<Job isLogueado={isLogueado} />} />
         <Route path='*' element={<NotFoundPage />} />
       </Routes>
       <Footer />
