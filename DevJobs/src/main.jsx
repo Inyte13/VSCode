a35b88eeaae3ad467737acf04279510042f85f1c
Inyte13 <luisgardaz13@gmail.com>
2026-01-03 23:28:26 -0500

    no auth provider, zustand

--- DevJobs/src/main.jsx
+++ DevJobs/src/main.jsx
@@ -1,13 +1,10 @@
 import { BrowserRouter } from 'react-router'
 import { createRoot } from 'react-dom/client'
-import { AuthProvider } from './context/AuthContext.jsx'
 import App from './App.jsx'
 import './App.css'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <App />
   </BrowserRouter>
 )
