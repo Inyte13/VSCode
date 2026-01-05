547f52645b221a56f58ea35f878c3db31f18d4e5
Inyte13 <luisgardaz13@gmail.com>
2026-01-05 17:19:20 -0500

    random

--- Python/NumPy/02_Random.py
+++ Python/NumPy/02_Random.py
@@ -5,6 +5,18 @@ os.system('cls')
 # randint, random enteros
 random.randint(10) # [0, 10)
 
-# rand
+# rand, random decimal entre 0 y 1
 random.rand() # [0, 1)
 
+# generando varios randints, size=(F x C)
+random.randint(10, size=(3, 3))
+# [[7 8 8]
+#  [1 2 7]
+#  [5 1 9]]
+
+# choice, elegir entre una lista
+nombres = ['Camilo', 'Lia', 'Luis', 'Cielo']
+random.choice(nombres) # Nombre aleatorio del vector nombres
+  
+# elegir varios entre una lista, PUEDE REPETIR
+random.choice(nombres, size=(2))
