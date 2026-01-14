edc377d224ffe4216d9af02c6e168a7db51fa97a
Inyte13 <luisgardaz13@gmail.com>
2026-01-13 23:05:37 -0500

    prueba-tecnica

--- prueba-tecnica/src/App.jsx
+++ prueba-tecnica/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '../../../vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
 
-function App () {
-  const [count, setCount] = useState(0)
+export default function App () {
+  const [hecho, setHecho] = useState()
+  // Por qué un useEffect?
+  // re: porque sino haría un fetch cada vez que se renderiza
+  //     componente y ese fetch tambien settearia mi hecho
+  //     haciendo un loop infinito
+  useEffect(() => {
+    fetch('https://catfact.ninja/fact')
+      .then(res => res.json()) // La respuesta la transformamos a json
+      .then(data => {
+        const fact = data.fact
+        // Lo mismo que escribir esto:
+        // const { fact } = data
+        // o así:
+        // const { fact: hecho } = data
+        setHecho(fact)
+        const primerasPalabras = fact.split(' ').slice(0, 3).join(' ')
+        // Lo mismo que escribir esto:
+        // const primerasPalabras = fact.split(' ', 3).join(' ')
+        console.log(primerasPalabras)
+      })
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href='https://vite.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <h1>App de gatitos</h1>
+      {hecho && (<p>{hecho}</p>)}
+    </main>
   )
 }
-
-export default App
